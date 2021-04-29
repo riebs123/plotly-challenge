f9e47ad14915d455a344e594c8126464944d12d5
riebs123 <tannerriebel@hotmail.com>
2021-04-28 21:11:54 -0500

    added dropdown

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,23 @@
 
+function InitDashboard() {
 
-d3.json('samples.json').then(function(data) {
-    console.log(data.samples);
+    var selector = d3.select('#selDataset');
+
+    d3.json('samples.json').then(function(data) {
+    console.log(data.names);
+
+    var sampleNames = data.names;
+
+    sampleNames.forEach(sampleId => {
+        console.log(sampleId)
+        selector.append('option')
+        .text(sampleId)
+        .property('value', sampleId);
+    })
 });
+    // update bar chart 
+    // update bubblechart
+
+}
+
+InitDashboard();
