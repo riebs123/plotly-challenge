256e4da975a2d2216c7d707ef31b957b5b2313fe
riebs123 <tannerriebel@hotmail.com>
2021-04-28 23:16:59 -0500

    bar graph added but not changing dynamically

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,22 +1,84 @@
 
+function DrawBargraph(sampleId) {
+    console.log(`DrawBargraph(${sampleId})`);
+
+    d3.json('samples.json').then (data => {
+        // console.log(data)
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id = sampleId);
+
+        var result = resultArray[0]
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuId => ` OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: 'bar',
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: 'h'
+        }
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: 'Top 10 Bacteria Cultures Found',
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot('bar', barArray, barLayout);
+
+    })
+
+};
+
+function DrawBubblechart(sampleId) {
+    console.log(`DrawBubblechart(${sampleId})`);
+
+}
+
+function ShowMetadata(sampleId) {
+    console.log(`ShowMetadata(${sampleId})`);
+
+}
+
+function optionChanged(newSampleId) {
+    console.log(`user selected  ${newSampleId}`);
+
+    DrawBargraph(newSampleId);
+    DrawBubblechart(newSampleId);
+    ShowMetadata(newSampleId);
+}
+
 function InitDashboard() {
 
     var selector = d3.select('#selDataset');
 
     d3.json('samples.json').then(function(data) {
-    console.log(data.names);
 
     var sampleNames = data.names;
 
     sampleNames.forEach(sampleId => {
-        console.log(sampleId)
         selector.append('option')
         .text(sampleId)
         .property('value', sampleId);
-    })
+    });
+    
+    var id = sampleNames[0];
+
+    DrawBargraph(id);
+
+    DrawBubblechart(id);
+
+    ShowMetadata(id);
+
 });
-    // update bar chart 
     // update bubblechart
+    //update demographic info
 
 }
 
